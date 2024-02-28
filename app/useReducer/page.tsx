e316bba002b6bbee5418adb04f71fecd8648deb8
Yuto Urashima <yuto.urashima24@gmail.com>
2024-02-28 21:06:41 +0900

    useReducerお試し

--- app/useReducer/page.tsx
+++ app/useReducer/page.tsx
@@ -1,7 +1,31 @@
-const SimpleCountUp = () => {
-  const initialState = 1
+"use client"
 
-  return <>test</>
+import { useReducer } from "react"
+
+const initialState = 1
+
+const countReducer = (state: number, action: string) => {
+  switch (action){
+    case "INCREMENT":
+      return state + 1
+    case "DECREMENT":
+      return state -1
+    case "RESET":
+      return initialState
+    default:
+      return state
+  }
+}
+
+const SimpleCountUp = () => {
+  const [state, dispatch] = useReducer(countReducer, initialState)
+
+  return <div>
+    <p>{state}</p>
+    <button onClick={() => dispatch("INCREMENT")}> + </button>
+    <button onClick={() => dispatch("DECREMENT")}> - </button>
+    <button onClick={() => dispatch("RESET")}> reset </button>
+  </div>
 }
 
 export default SimpleCountUp
